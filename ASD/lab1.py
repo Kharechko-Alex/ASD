6830de8d175656b5869bf4b74ee2fdb42c0ab595
Alex <sashakharechko26@gmail.com>
2023-02-27 23:26:06 +0200

    add plot building and project refactored

--- ASD/lab1.py
+++ ASD/lab1.py
@@ -1,46 +1,156 @@
 from random import uniform
 from time import perf_counter
-
+import matplotlib.pyplot as plt
 
 def main():
-    a = [round(uniform(-10, 10), 4) for _ in range(1000)]
+    a = [round(uniform(0, 100), 4) for _ in range(10)]
+    text_info(a)
+    size = (10, 100, 1000)
+    for i in size:
+        ...
+        plot_building(i, a)
+
+
+def plot_building(size, a):
+    plt.title('Bubble sort')
+    plt.ylabel('Operations')
+    plt.xlabel('Array size')
+    x_axis = []
+    y_axis = [[], [], []]
+
+    for i in range(1, size+1):
+        arr = gen_arr(i)
+        x_axis.append(i)
+        y_axis[0].append(bubble_sort(arr[:])[1]+bubble_sort(arr[:])[2])
+        y_axis[1].append(bubble_sort(sorted(arr[:]))[1]+bubble_sort(sorted(arr[:]))[2])
+        y_axis[2].append(bubble_sort(sorted(arr[:], reverse=True))[1]+bubble_sort(sorted(arr[:], reverse=True))[2])
+
+    plt.plot(x_axis, y_axis[0], color='red')
+    plt.plot(x_axis, y_axis[1], color='green')
+    plt.plot(x_axis, y_axis[2], color='purple')
+    plt.plot(x_axis, [i - 1 for i in range(1, size + 1)], color='yellow', linewidth=2)
+    # plt.plot(x_axis, [i ** 2 for i in range(1, size + 1)], color='blue')
+    plt.legend(("randomized", "sorted", "reversed", "best variant", "worst variant"))
+    plt.show()
+
+    # plot for upgraded bubble sort
+    plt.title('Upgraded Bubble sort')
+    plt.ylabel('Operations')
+    plt.xlabel('Array size')
+    x_axis = []
+    y_axis = [[], [], []]
+
+    for i in range(1, size+1):
+        arr = gen_arr(i)
+        x_axis.append(i)
+        y_axis[0].append(upgraded_bubble_sort(arr[:])[1]+upgraded_bubble_sort(arr[:])[2])
+        y_axis[1].append(upgraded_bubble_sort(sorted(arr[:]))[1]+upgraded_bubble_sort(sorted(arr[:]))[2])
+        y_axis[2].append(upgraded_bubble_sort(sorted(arr[:], reverse=True))[1]+upgraded_bubble_sort(sorted(arr[:], reverse=True))[2])
+
+    plt.plot(x_axis, y_axis[0], color='red')
+    plt.plot(x_axis, y_axis[1], color='green')
+    plt.plot(x_axis, y_axis[2], color='purple')
+    plt.plot(x_axis, [i - 1 for i in range(1, size + 1)], color='yellow', linewidth=2)
+    # plt.plot(x_axis, [i ** 2 for i in range(1, size + 1)], color='blue')
+    plt.legend(("randomized", "sorted", "reversed", "best variant", "worst variant"))
+    plt.show()
+
+    # plot for insertion sort algorithm
+    plt.title('Insertion sort')
+    plt.ylabel('Operations')
+    plt.xlabel('Array size')
+    x_axis = []
+    y_axis = [[], [], []]
+
+    for i in range(1, size+1):
+        arr = gen_arr(i)
+        x_axis.append(i)
+        y_axis[0].append(insertion_sort(arr[:])[1]+insertion_sort(arr[:])[2])
+        y_axis[1].append(insertion_sort(sorted(arr[:]))[1]+insertion_sort(sorted(arr[:]))[2])
+        y_axis[2].append(insertion_sort(sorted(arr[:], reverse=True))[1]+insertion_sort(sorted(arr[:], reverse=True))[2])
+
+    plt.plot(x_axis, y_axis[0], color='red')
+    plt.plot(x_axis, y_axis[1], color='green')
+    plt.plot(x_axis, y_axis[2], color='purple')
+    plt.plot(x_axis, [i - 1 for i in range(1, size + 1)], color='yellow', linewidth=2)
+    # plt.plot(x_axis, [i ** 2 for i in range(1, size + 1)], color='blue')
+    plt.legend(("randomized", "sorted", "reversed", "best variant", "worst variant"))
+    plt.show()
+
+def text_info(a):
     print("Random generated array: \n", a)
     start = perf_counter()
-    print(bubble_sort(a[:]))
+    print("Sorted array: ")
+    print(bubble_sort(a[:])[0], end='\n\n')
+
     print(f"Sorted time for Bubble function on random array: \n{perf_counter()-start} sec")
-
+    print("Number of comprasion: ", bubble_sort(a[:])[1])
+    print("Number of permutation: ", bubble_sort(a[:])[2], end='\n\n')
     start = perf_counter()
-    print(bubble_sort(sorted(a[:])))
+    # print(bubble_sort(sorted(a[:]))[0])
     print(f"Sorted time for Bubble function on full-sorted array: \n{perf_counter()-start} sec")
-
+    print("Number of comprasion: ", bubble_sort(sorted(a[:]))[1])
+    print("Number of permutation: ", bubble_sort(sorted(a[:]))[2], end='\n\n')
     start = perf_counter()
-    print(bubble_sort(sorted(a[:], reverse = True)))
+    # print(bubble_sort(sorted(a[:], reverse=True))[0])
     print(f"Sorted time for Bubble function on reversed-sorted array: \n{perf_counter()-start} sec")
+    print("Number of comprasion: ", bubble_sort(sorted(a[:], reverse=True))[1])
+    print("Number of permutation: ", bubble_sort(sorted(a[:], reverse=True))[2], end='\n\n')
+
     print("\n------------------------------------------\n")
 
     start = perf_counter()
-    print(upgraded_bubble_sort(a[:]))
-    print(f"Sorted time for Upgraded Bubble function: {perf_counter()-start} sec")
-    print("\n------------------------------------------\n")
+    # print(upgraded_bubble_sort(a[:]))
+    print(f"Sorted time for Upgraded Bubble function on random array: \n{perf_counter()-start} sec")
+    print("Number of comprasion: ", upgraded_bubble_sort(a[:])[1])
+    print("Number of permutation: ", upgraded_bubble_sort(a[:])[2], end='\n\n')
     start = perf_counter()
-    print(insertion_sort(a[:]))
-    print(f"Sorted time for insertion sort function: {perf_counter()-start} sec")
+    # print(upgraded_bubble_sort(sorted(a[:]))[0])
+    print(f"Sorted time for Upagraded Bubble sort function on full-sorted array: \n{perf_counter()-start} sec")
+    print("Number of comprasion: ", upgraded_bubble_sort(sorted(a[:]))[1])
+    print("Number of permutation: ", upgraded_bubble_sort(sorted(a[:]))[2], end='\n\n')
+    start = perf_counter()
+    # print(upgraded_bubble_sort(sorted(a[:], reverse=True))[0])
+    print(f"Sorted time for Upgraded Bubble sort function on reversed-sorted array: \n{perf_counter()-start} sec")
+    print("Number of comprasion: ", upgraded_bubble_sort(sorted(a[:], reverse=True))[1])
+    print("Number of permutation: ", upgraded_bubble_sort(sorted(a[:], reverse=True))[2], end='\n\n')
+
+    print("\n------------------------------------------\n")
+
+    start = perf_counter()
+    # print(insertion_sort(a[:]))
+    print(f"Sorted time for Insertion sort function on random array: \n{perf_counter()-start} sec")
+    print("Number of comprasion: ", insertion_sort(a[:])[1])
+    print("Number of permutation: ", insertion_sort(a[:])[2], end='\n\n')
+    start = perf_counter()
+    # print(insertion_sort(sorted(a[:]))[0])
+    print(f"Sorted time for Insertion sort function on full-sorted array: \n{perf_counter()-start} sec")
+    print("Number of comprasion: ", insertion_sort(sorted(a[:]))[1])
+    print("Number of permutation: ", insertion_sort(sorted(a[:]))[2], end='\n\n')
+    start = perf_counter()
+    # print(insertion_sort(sorted(a[:], reverse=True))[0])
+    print(f"Sorted time for Insertion sort function on reversed-sorted array: \n{perf_counter()-start} sec")
+    print("Number of comprasion: ", insertion_sort(sorted(a[:], reverse=True))[1])
+    print("Number of permutation: ", insertion_sort(sorted(a[:], reverse=True))[2], end='\n\n')
+
+
+def gen_arr(len):
+    return [uniform(-10, 10) for _ in range(len)]
 
 
 def bubble_sort(list_a):
     number_of_comparison = 0
     number_of_permutation = 0
 
-    for i in range(len(list_a)-1):
+    for i in range(len(list_a) - 1):
 
         for j in range(len(list_a) - i - 1):
 
             number_of_comparison += 1
-            if list_a[j] > list_a[j+1]:
+            if list_a[j] > list_a[j + 1]:
                 number_of_permutation += 1
-                list_a[j], list_a[j+1] = list_a[j+1], list_a[j]
-    print()
-    return list_a
+                list_a[j], list_a[j + 1] = list_a[j + 1], list_a[j]
+    return list_a, number_of_comparison, number_of_permutation
 
 
 def upgraded_bubble_sort(array):
@@ -61,7 +171,7 @@ def upgraded_bubble_sort(array):
         if swapped == False:
             break
 
-    return array
+    return array, number_of_comparison, number_of_permutation
 
 
 def insertion_sort(array):
@@ -79,8 +189,7 @@ def insertion_sort(array):
             j -= 1
         array[j + 1] = key
 
-    return array
-
+    return array, number_of_comparison, number_of_permutation
 
 
 if __name__ == "__main__":
